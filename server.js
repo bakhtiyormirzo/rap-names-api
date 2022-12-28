5d69ca121ecb83c10ab1f0a41543a34f85bf87d5
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2022-12-28 13:12:33 +0300

    express api

--- server.js
+++ server.js
@@ -1,41 +1,71 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const PORT = 8000
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
 
-app.use(cors())
 
-const rappers = {
-    '21 savage':{
-        'age': 29,
-        'birthName':'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England' 
-    },
-    'chance the rapper':{
-        'age': 29,
-        'birthName':'Chancelor Bennett',
-        'birthLocation': 'Chicago, Illinois' 
-    },
-    'dylan':{
-        'age': 29,
-        'birthName':'Dylan',
-        'birthLocation': 'Dylan' 
-    }
-}
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'rap'
 
-app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/index.html')
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/',(request, response)=>{
+    db.collection('rappers').find().sort({likes: -1}).toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
 })
 
-app.get('/api/:rapperName', (request,response)=>{
-    const rappersName = request.params.rapperName.toLowerCase()
-    if(rappers[rappersName]){
-        response.json(rappers[rappersName])
-    }else{
-        response.json(rappers['dylan'])
-    }
+app.post('/addRapper', (request, response) => {
+    db.collection('rappers').insertOne({stageName: request.body.stageName,
+    birthName: request.body.birthName, likes: 0})
+    .then(result => {
+        console.log('Rapper Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+          }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteRapper', (request, response) => {
+    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+    .then(result => {
+        console.log('Rapper Deleted')
+        response.json('Rapper Deleted')
+    })
+    .catch(error => console.error(error))
+
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+    console.log(`Server running on port ${PORT}`)
 })
